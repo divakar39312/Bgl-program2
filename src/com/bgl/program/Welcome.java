0481d6d72529238db43716d3ad248a4870622df3
Divakar <divakar39312@gmail.com>
2022-07-20 10:37:32 +0530

    second commit

--- src/com/bgl/program/Welcome.java
+++ src/com/bgl/program/Welcome.java
@@ -3,5 +3,14 @@ package com.bgl.program;
 public class Welcome {
 public static void main(String[] args) {
 	System.out.println("welcome to Employee wage computation program");
+	
+	int isfulltime=1;
+	double empCheck=Math.floor(Math.random()*10)%2;
+		if(empCheck==isfulltime) 
+			System.out.println("Employee is present");
+			else
+				System.out.println("employee is absent");
+		
+		
 }
 }
