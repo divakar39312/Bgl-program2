37417a6a10176397182f6cf9249bb2dda2ef1f6e
Divakar <divakar39312@gmail.com>
2022-07-20 11:23:05 +0530

    fourth commit

---
+++ src/com/bgl/program/Empwageswitchcase.java
@@ -0,0 +1,29 @@
+package com.bgl.program;
+
+public class Empwageswitchcase {
+
+	public static final int isparttime=1;
+	public static final int isfulltime=2;
+	public static final int Emprateperhr=20;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int empHrs=0;
+       int empwage=0;
+       int empCheck=(int)Math.floor(Math.random()*10)%3;
+       switch(empCheck) {
+       case isparttime:
+    	   empHrs=4;
+    	   break;
+       case isfulltime:
+           empHrs=8;
+           break;
+         default:
+        	 empHrs=0;
+       }
+       empwage=empHrs*Emprateperhr;
+       System.out.println("empwage"+empwage);
+		
+		
+	}
+
+}
