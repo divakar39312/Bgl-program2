033bb53fe962607e95bc213913438f15b425c9b7
Divakar <divakar39312@gmail.com>
2022-07-21 14:57:58 +0530

    fifth commit

---
+++ src/com/bgl/program/WageforMonth.java
@@ -0,0 +1,34 @@
+package com.bgl.program;
+
+public class WageforMonth {
+	public static final int isparttime=1;
+	public static final int isfulltime=2;
+	public static final int emprateperhrs=20;
+	public static final int numofworkingdays=2;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int empHrs=0;
+      int empwage=0;
+      int totalEmpwage=0;
+      for(int day=0;day<numofworkingdays;day++) {
+    	  int empCheck=(int) Math.floor(Math.random()*10)%3;
+      switch (empCheck) {
+      case isparttime:
+        empHrs=4;
+        break;
+      case isfulltime:
+    	  empHrs=8;
+    	  break;
+    	  default:
+    		  empHrs=0;
+      }
+      empwage= empHrs*emprateperhrs;
+      totalEmpwage+=empwage;
+      System.out.println("empwage"+empwage);
+      }
+      System.out.println("total emp wage"+totalEmpwage);
+      }
+	}
+
+
